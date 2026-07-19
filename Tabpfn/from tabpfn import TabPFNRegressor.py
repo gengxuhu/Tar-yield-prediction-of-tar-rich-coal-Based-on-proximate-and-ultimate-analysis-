b82ba7a435890c1ef8cfce3888f3643324e038c1
gengxuhu <156675003+gengxuhu@users.noreply.github.com>
2026-07-19 16:23:39 +0800

    Delete Tabpfn directory

--- Tabpfn/from tabpfn import TabPFNRegressor.py
+++
@@ -1,2 +0,0 @@
-from tabpfn import TabPFNRegressor
-print(TabPFNRegressor._get_param_names())  # 打印所有有效参数
--- Tabpfn/gpu.py
+++
@@ -1,3 +0,0 @@
-import torch
-print(torch.cuda.is_available())  # 应输出True
-print(torch.cuda.device_count())  # 显示可用GPU数量
